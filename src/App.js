5ab9976534112115cf6fda7cd1e3ef4e34457227
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-16 12:37:05 +0600

    reactjs core concept dynamically data load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
+
     </div>
   );
 }
+
+function ExternalUsers () {
+  const [users, setUsers] = useState([]);
+  useEffect(()=> {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  } , [])
+
+
+  return (
+    <div>
+      <h2 className='data-title'>External Users</h2>
+      <br />
+      <p className='total-data'>Total Data: {users.length}</p>
+      {
+        users.map( user => <User name = {user.name} email = {user.email} website = {user.website}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div className='user-info'>
+      <h3>Name: {props.name}</h3>
+      <h3>Email: {props.email}</h3>
+      <h3>Website: {props.website}</h3>
+    </div>
+  )
+}
+
+
+
+
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
 
 export default App;
